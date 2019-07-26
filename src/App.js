72933bd1808549ca1054a9ef80103ec09d11fc6e
Mahdieh <mahdieh.jamshidi69@gmail.com>
2019-07-27 03:36:23 +0430

    submitHandler

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      firstname: [],
-      lastname: [],
-      username: []
+      orders: []
     }
   }
 
-  clickHandler = () =>{
-    this.setState();
+  submitHandler = (event) =>{
+    event.preventDefault();
+    const order = {
+      firstname:document.getElementsByName("firstname")[0].value,
+      lastname:document.getElementsByName("lastname")[0].value,
+      username:document.getElementsByName("username")[0].value,
+      gender:document.getElementsByName("gender")[0].value
+    }
+    const {orders} = this.state;
+    this.setState({
+      orders: [...orders,order]
+    });
   }
 
 
   render(){
     return(
       <div className="main">
-        <div className="firstrow">
+        <form className="firstrow" onSubmit={this.submitHandler}>
         <div className="entrace">
           <h5>First Name</h5>
           <input type="text" name="firstname" placeholder="First Name*"/>
@@ -38,17 +46,20 @@ export default class App extends Component {
         </div>
         <div className="entrace">
           <h5>Action</h5>
-          <button onClick={this.clickHandler}>Add</button>
+          <button type="submit">Add</button>
         </div>
-        </div>
+        </form>
         <ul className="order-list">
-          {/* {this.state.orders.map((order,index)=><li className="order-item" key={index}>
-          <span>{firstname}</span>
-          <span>{lastname}</span>
-          <span>{username}</span>
-          
+          {this.state.orders.map((order,index)=><li className="order-item" key={index}>
+          <span>{order.firstname}</span>
+          <span></span>
+          <span>{order.lastname}</span>
+          <span></span>
+          <span>{order.username}</span>
+          <span></span>
+          <span>{order.gender}</span>
           <button>X</button>
-          </li>)} */}
+          </li>)}
         </ul>
       </div>
     )
