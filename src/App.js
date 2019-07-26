f2cb7de263a133422d128154ee53e94a9a383a92
Mahdieh <mahdieh.jamshidi69@gmail.com>
2019-07-27 01:09:03 +0430

    input row

--- src/App.js
+++ src/App.js
@@ -2,86 +2,53 @@ import React,{Component} from 'react';
 import "./App.css";
 
 export default class App extends Component {
-  
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
-      toDo: '',
-      toDoList: [],
-      doneList: []
+      firstname: [],
+      lastname: [],
+      username: []
     }
   }
 
-  submitHandler = (event) =>{
-    event.preventDefault();
-    const {toDoList,toDo} = this.state; // const toDoList = this.state.toDoList;
-    this.setState({
-      toDoList:[...toDoList, {text:toDo, id:new Date().getTime()}],
-      toDo: ''
-    })
+  clickHandler = () =>{
+    this.setState();
   }
 
-  changeHandler = (event) =>{
-    const {name,value} = event.target;
-    this.setState({[name]: value});
-  }
 
-  // deleteHandler = (id) =>{
-  //   return ()=>{
-      
-  //   }
-  // }
-
-  deleteHandler = id => ()=> {
-    const {toDoList} = this.state;
-    const idx = toDoList.findIndex(toDo=>toDo.id === id);
-    toDoList.splice(idx,1);
-    this.setState({toDoList});
-  }
-
-  checkBoxChange = (event) =>{
-    const {checked} = event.target;
-    if (checked) {
-      const id = event.target.getAttribute('data-id');
-      const {toDoList,doneList} = this.state; // const toDoList = this.state.toDoList;
-      const idx = toDoList.findIndex(toDo=>toDo.id == id);
-      const done = toDoList.splice(idx,1);
-      this.setState({
-        toDoList,
-        doneList: [...doneList,...done]
-      })
-    } else {
-      const id = event.target.getAttribute('data-id');
-      const {toDoList,doneList} = this.state; // const toDoList = this.state.toDoList;
-      const idx = doneList.findIndex(toDo=>toDo.id == id);
-      const done = doneList.splice(idx,1);
-      this.setState({
-        doneList,
-        toDoList: [...toDoList,...done]
-      })
-    }
-  }
-
-  render() {
-    return (
-      <div style={{display: 'flex', alignItems:'center',flexDirection: 'column',width: 400,margin:'auto'}}>
-        <h1>Keep List</h1>
-        <form style={{display:'flex',width:'100%'}} onSubmit={this.submitHandler}>
-          <input onChange={this.changeHandler} value={this.state.toDo} type="text" name="toDo" placeholder="toDo" className="input"/>
-          <button type="submit" disabled={!this.state.toDo}>Add</button>
-        </form>
-        <ul className="toDo-list">
-          {this.state.toDoList.map(toDo=><li className="toDo-item" key={toDo.id}>
-            <input type="checkbox" data-id={toDo.id} onChange={this.checkBoxChange} />
-            <span>{toDo.text}</span>
-            <button onClick={this.deleteHandler(toDo.id)} >X</button>
-          </li>)}
-        </ul>
-        <ul className="toDo-list">
-          {this.state.doneList.map(toDo=><li className="toDo-item" key={toDo.id}>
-            <input type="checkbox" data-id={toDo.id} onChange={this.checkBoxChange} checked />
-            <span>{toDo.text}</span>
-          </li>)}
+  render(){
+    return(
+      <div className="main">
+        <div className="firstrow">
+        <div className="entrace">
+          <h5>First Name</h5>
+          <input type="text" name="firstname" placeholder="First Name*"/>
+        </div>
+        <div className="entrace">
+          <h5>Last Name</h5>
+          <input type="text" name="lastname" placeholder="Last Name*"/>
+        </div>
+        <div className="entrace">
+          <h5>Username</h5>
+          <input type="text" name="username" placeholder="Username*"/>
+        </div>
+        <div className="entrace">
+          <h5>Gender</h5>
+          <input type="checkbox" name="gender"/>
+        </div>
+        <div className="entrace">
+          <h5>Action</h5>
+          <button onClick={this.clickHandler}>Add</button>
+        </div>
+        </div>
+        <ul className="order-list">
+          {/* {this.state.orders.map((order,index)=><li className="order-item" key={index}>
+          <span>{firstname}</span>
+          <span>{lastname}</span>
+          <span>{username}</span>
+          
+          <button>X</button>
+          </li>)} */}
         </ul>
       </div>
     )
